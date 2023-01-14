1795d5ba2628eb7d47bd0913f30dd4e4c2e3d412
ralphwright <ralphwrightengineer@gmail.com>
2023-01-14 16:28:06 -0600

    syntactic changes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.get('/', (req, res) => {
     res.send({hi: 'there'});
 })
 
-app.listen(5000);
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
